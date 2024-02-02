19380af456e13590e112b2537604a3f32de273dc
AlessioDiMari <alessiodimari.adm25@gmail.com>
2024-02-02 17:59:05 +0100

    inizio primo snack

---
+++ jsnack1/script.js
@@ -0,0 +1,12 @@
+/*
+L’utente inserisce due parole in successione, con due prompt.
+Il software stampa prima la parola più corta, poi la parola più lunga.
+*/
+
+
+
+alert("Benvenuto nello snack 1");
+
+const firstWord = prompt("Insegisci la prima parola");
+
+const secondWord = prompt("Insegisci la seconda parola");
