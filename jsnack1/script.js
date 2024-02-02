e39c99ec25c6224ad189a68a48b57b6c2655083d
AlessioDiMari <alessiodimari.adm25@gmail.com>
2024-02-02 18:08:37 +0100

    richiedo le parole all'utente e assegno variabili

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -4,9 +4,20 @@ Il software stampa prima la parola più corta, poi la parola più lunga.
 */
 
 
-
+// Do il benvenuto all'utente
 alert("Benvenuto nello snack 1");
 
+
+// Richiedo le parole all'utente
 const firstWord = prompt("Insegisci la prima parola");
 
 const secondWord = prompt("Insegisci la seconda parola");
+
+
+// Do una variabile per verificare la lunghezza
+// delle parole che l'utente ha inserito
+const firstLength = firstWord.length;
+console.log(firstLength);
+
+const secondLength = secondWord.length;
+console.log(secondLength);
