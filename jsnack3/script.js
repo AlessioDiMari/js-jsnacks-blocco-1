dfa9cf43ce8064cab2f362a083db3d123bfc4047
AlessioDiMari <alessiodimari.adm25@gmail.com>
2024-02-04 16:05:30 +0100

    creo un array vuoto e lo faccio riempire all'utente

---
+++ jsnack3/script.js
@@ -0,0 +1,21 @@
+/*
+Crea un array vuoto.
+Chiedi per 6 volte all’utente di inserire un numero,
+Solo se è dispari inseriscilo nell’array.
+Stampa a schermo il contenuto dell'array
+*/
+
+alert("Benvenuto nel terzo snack");
+
+// Creo l'array vuoto
+let oddsNumbers = [];
+
+// Creo un ciclo for in cui faccio inserire
+// 6 numeri all'utente
+for ( let i = 0; i < 6; i++){
+    
+    let input = parseInt(prompt("Inserisci un numero"));
+
+    oddsNumbers.push(input);
+}
+console.log(oddsNumbers);
