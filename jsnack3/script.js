e05ec7a333627200165c604b206c309dd59bba34
AlessioDiMari <alessiodimari.adm25@gmail.com>
2024-02-04 17:01:07 +0100

    inserisco nell'array solo i numeri dispari

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -1,9 +1,7 @@
-/*
-Crea un array vuoto.
+/* Crea un array vuoto.
 Chiedi per 6 volte all’utente di inserire un numero,
 Solo se è dispari inseriscilo nell’array.
-Stampa a schermo il contenuto dell'array
-*/
+Stampa a schermo il contenuto dell'array */
 
 alert("Benvenuto nel terzo snack");
 
@@ -16,6 +14,9 @@ for ( let i = 0; i < 6; i++){
     
     let input = parseInt(prompt("Inserisci un numero"));
 
-    oddsNumbers.push(input);
+    // creo un if per far inserire solo i numeri dispari
+    if ( input % 2 != 0){
+        oddsNumbers.push(input);
+    }
 }
 console.log(oddsNumbers);
